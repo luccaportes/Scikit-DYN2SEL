574d524695de53cf82f6b8d20eacb42c95c48990
luccaportes <lucca54@gmail.com>
2020-08-06 14:15:57 +0200

    test dynse

--- dyn2sel/tests/apply_dcs/test_dynse.py
+++ dyn2sel/tests/apply_dcs/test_dynse.py
@@ -1,9 +1,7 @@
-import sys
-sys.path.append("..")
 from skmultiflow.data import SEAGenerator
 from skmultiflow.bayes import NaiveBayes
 from dyn2sel.apply_dcs import DYNSEMethod
-from dyn2sel.dcs_techniques import KNORAE
+from dyn2sel.dcs_techniques import METADES, ModifiedRank
 
 
 def test_ensemble_size():
@@ -13,7 +11,22 @@ def test_ensemble_size():
     n_samples = 1050
     gen = SEAGenerator()
     gen.prepare_for_use()
-    dynse = DYNSEMethod(NaiveBayes(), chunk_size, KNORAE())
+    dynse = DYNSEMethod(NaiveBayes(), chunk_size, ModifiedRank())
     X, y = gen.next_sample(n_samples)
     dynse.partial_fit(X, y)
     assert len(dynse.ensemble) == n_samples // chunk_size
+
+
+def test_accuracy():
+    # an ensemble of Naive Bayes should perform at the very least 85% with 200 instances of SEAGenerator
+    chunk_size = 100
+    n_samples_train = 1050
+    n_samples_test = 200
+    gen = SEAGenerator()
+    gen.prepare_for_use()
+    nb = NaiveBayes()
+    mde = DYNSEMethod(nb, chunk_size, METADES())
+    X_train, y_train = gen.next_sample(n_samples_train)
+    X_test, y_test = gen.next_sample(n_samples_test)
+    mde.partial_fit(X_train, y_train)
+    assert mde.score(X_test, y_test) > 0.85

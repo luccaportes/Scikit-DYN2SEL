5bdb6cfdc48ee0416bd8ce8869dbe24b7caf2f9f
Lucca Portes <lucca54@gmail.com>
2021-09-10 22:45:49 -0300

    new release

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(
     name="dyn2sel",  # How you named your package folder (MyLib)
@@ -13,24 +12,25 @@ setup(
         "dyn2sel.utils",
         "dyn2sel.utils.evaluators",
     ],  # Chose the same as "name"
-    version="0.1.3",  # Start with a small number and increase it with every change you make
+    version="0.1.5",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="A framework for applying and implementing DCS techniques in the data stream mining context.",  # Give a short description about your library
     author="Lucca Portes",  # Type in your name
     author_email="lucca54@gmail.com",  # Type in your E-Mail
     url="https://github.com/luccaportes/Scikit-DYN2SEL",  # Provide either the link to your github or to your website
-    download_url="https://github.com/luccaportes/Scikit-DYN2SEL/archive/v_013.tar.gz",  # I explain this later on
+    download_url="https://github.com/luccaportes/Scikit-DYN2SEL/archive/v_015.tar.gz",  # I explain this later on
     keywords=[
         "Dynamic Selection",
         "Machine Learning",
         "Data Streams",
     ],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
-        "numpy",
-        "scikit-learn",
-        "scikit-multiflow",
-        "scipy",
-        "deslib",
+        "numpy==1.18.1",
+        "scikit-learn==0.22.1",
+        "scikit-multiflow==0.4.1",
+        "scipy==1.4.1",
+        "deslib==0.3",
+        "imblearn"
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
